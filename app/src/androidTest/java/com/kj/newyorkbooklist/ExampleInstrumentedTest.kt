d7de2e6ab69ad80a591f307ce9efd62be7e4a3b4
Kushagra <kushagra.jaiswal.cer13@itbhu.ac.in>
2022-01-08 11:46:09 +0530

    Renamed package structure

--- app/src/androidTest/java/com/kj/newyorkbooklist/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/kj/newyorkbooklist/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.newyorkbooklist
+package com.kj.newyorkbooklist
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
